5260648fa34b8a81892a9ea50841f4d7a0a0d6a2
vaishu-shann <ilukiabi@gmail.com>
2022-08-31 22:00:13 +0530

    Add hero section dialog box.

--- src/components/HeroSection/hero.js
+++ src/components/HeroSection/hero.js
@@ -1,46 +1,79 @@
-import React from 'react';
-import "./hero.css"
-import BannerImage from "../../assests/images/started-main.png"
-import Fade from 'react-reveal/Fade';
-import 'antd/dist/antd.css';
-import { Button } from 'antd';
-import { AiFillPlayCircle } from 'react-icons/ai';
+import React, { useState } from "react";
+import "./hero.css";
+import BannerImage from "../../assests/images/started-main.png";
+import Rocket from "../../assests/images/rocket.png";
+import Fade from "react-reveal/Fade";
+import "antd/dist/antd.css";
+import { Button, Input, Modal } from "antd";
+import { AiFillPlayCircle } from "react-icons/ai";
 import { IconContext } from "react-icons";
 
 function HeroSection(props) {
-    return (
-      
-        <div className='hero-section'>
-         
-            <div className='left-cont'>
-            <Fade left>
-                <h1 className='hero-section-header'>HODL, EARN, SPEND</h1> </Fade>
-                <Fade left>
-                <p>A crypto payment app that makes your Fiat &#60; &#62; Crypto transfers &amp; spendings much easier and seamless than ever.</p></Fade>
-                <Fade left> <div className='get-playstore'>
-                       <Button type="primary" className='join-early'>Join Early Access</Button>
-                       <Button type="primary" className='how-work'>
-                        <div style={{display:'flex',alignItem:'center'}}>
-                       <IconContext.Provider
-                value={{
-                  size: "1.5em",
-                  color: "#0f2310",    
-                  style:{marginRight:5}  ,         
-                  className: "global-class-name",
-                }}
-              >
-              <AiFillPlayCircle/>
-              </IconContext.Provider>
-                        How CrypIT works </div></Button>
-                </div></Fade>
-            </div>
-           
-            <div className='right-cont'>
-                <img className='hero-img' src={BannerImage} alt="hero "/>
-            </div>
-        </div>
-       
-    );
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <div className="hero-section">
+      <div className="left-cont">
+        <Fade left>
+          <h1 className="hero-section-header">HODL, EARN, SPEND</h1>{" "}
+        </Fade>
+        <Fade left>
+          <p>
+            A crypto payment app that makes your Fiat &#60; &#62; Crypto
+            transfers &amp; spendings much easier and seamless than ever.
+          </p>
+        </Fade>
+        <Fade left>
+          {" "}
+          <div className="get-playstore">
+            <Button type="primary" className="join-early" onClick={showModal}>
+              Join Early Access
+            </Button>
+            <Button type="primary" className="how-work">
+              <div style={{ display: "flex", alignItem: "center" }}>
+                <IconContext.Provider
+                  value={{
+                    size: "1.5em",
+                    color: "#0f2310",
+                    style: { marginRight: 5 },
+                    className: "global-class-name",
+                  }}
+                >
+                  <AiFillPlayCircle />
+                </IconContext.Provider>
+                How CrypIT works{" "}
+              </div>
+            </Button>
+          </div>
+        </Fade>
+      </div>
+      <Modal
+        visible={isModalVisible}
+        onOk={handleOk}
+        okText="Submit"
+        cancelButtonProps={{ style: { display: "none" } }}
+      >
+        <img className="Rocket-img" src={Rocket} alt="Rocket"/>
+        <h3 style={{textAlign:'center',marginBottom:"20px"}}>Enter your Email Address</h3>
+        <Input placeholder="Email Id" />
+      </Modal>
+
+      <div className="right-cont">
+        <img className="hero-img" src={BannerImage} alt="hero " />
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
