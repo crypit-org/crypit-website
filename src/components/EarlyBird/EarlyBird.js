6fd9975a47a7b52a4c61308fdfc4c1b5ee311463
vaishu-shann <ilukiabi@gmail.com>
2022-11-18 21:45:59 +0530

    Early bird section UI enhancement.

--- src/components/EarlyBird/EarlyBird.js
+++ src/components/EarlyBird/EarlyBird.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./earlybird.css"
+import Block1 from "../../assets/img-2.0/block1.png"
+import Block2 from "../../assets/img-2.0/block2.png"
+import JustCrypit from "../../assets/img-2.0/Just Cryp IT.png"
 
 function EarlyBird(props) {
     return (
@@ -11,6 +14,9 @@ function EarlyBird(props) {
         <input className='input-email' placeholder='Email'  type='email'/>
         <input className='input-email' placeholder='Phone Number'  type='number'/>
         <div className='email-join-cta'>Join Early Access</div>
+<img src={Block1} alt="" className='Block1' />
+     <img src={Block2} alt="" className='Block2' />
+     <img src={JustCrypit} alt="" className='JustCrypit' />
      
      </div>
         </div>
