14950a81d0dd47669cfeeb885b2be85e5d86cd3d
vaishu-shann <ilukiabi@gmail.com>
2022-11-18 20:57:13 +0530

    Download App section UI changes.

--- src/components/Download/Download.js
+++ src/components/Download/Download.js
@@ -5,14 +5,18 @@ import MobileVector from "../../assets/img-2.0/mob-overview.svg"
 function Download(props) {
     return (
         <div className='download-container'>
-            <div className='back-50'>
-            <div className='feature-sec-heading'>Download App</div>
-     <div className='feature-sec-underline'/>
-            </div>
-            <img src={MobileVector} alt="mobile" className='mob1' />
-            <img src={MobileVector} alt="mobile" className='mob2' />
-            <img src={MobileVector} alt="mobile" className='mob3' />
+                {/* <div className='back-50'>
+                <div className='feature-sec-heading'>Download App</div>
+        <div className='feature-sec-underline'/>
+                </div>
+                <img src={MobileVector} alt="mobile" className='mob1' />
+                <img src={MobileVector} alt="mobile" className='mob2' />
+                <img src={MobileVector} alt="mobile" className='mob3' /> */}
             <div className='back-100'>
+                <div className='dwl-app-title'>
+                 <div className='dwl-app-feature-sec-heading'>Download App</div>
+     <div className='underline'/>
+     </div>
                 <div className='download-sec'>
                     <div className='description-detail'>
                     Download the app today to experience the world of crypto at your fingertips!
